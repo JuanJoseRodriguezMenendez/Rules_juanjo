2bdf737141dfea520f4e1d6475ae4ca610cc5161
salvador.penalva@bosonit.com <salvador.penalva@bosonit.com>
2018-03-12 18:30:30 +0100

    Revert "Token-utilities: added use of tokens"
    This reverts commit 6b3fdc028473e761db1cdb68d23acdc02dfd65a8.

--- src/main/java/restAPI/PerseoController.java
+++ src/main/java/restAPI/PerseoController.java
@@ -7,6 +7,7 @@ import java.nio.file.Files;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,8 +19,8 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.internal.LinkedTreeMap;
 
-import auth.AuthUtils;
 import fiwoo.microservices.rules_External_Actions.fiwoo_rules_External_Actions.Logic;
+import io.swagger.annotations.BasicAuthDefinition;
 
 
 @RestController
@@ -32,10 +33,9 @@ public class PerseoController {
 	}
 	
 	// Get Methods
-	@RequestMapping(method = RequestMethod.GET, value = "/statements", headers="Accept=application/json")
-	public ResponseEntity getRules() {
-		auth.model.User loggedUser = AuthUtils.getUser();	
-		String result = logic.getRulesOfUser(loggedUser.getId().toString()); 
+	@RequestMapping(method = RequestMethod.GET, value = "/statements/{user_id}", headers="Accept=application/json")
+	public ResponseEntity getRules(@PathVariable("user_id") String user_id) {
+		String result = logic.getRulesOfUser(user_id); 
 		return ResponseEntity.status(HttpStatus.OK).body(result);
 	}
     
@@ -90,7 +90,6 @@ public class PerseoController {
 	@RequestMapping(value = "/statements/advanced/add", method = RequestMethod.POST, headers="Accept=application/json", consumes = {"application/json"})
 	@ResponseBody
 	public ResponseEntity addRule(@RequestBody String body) {
-		auth.model.User loggedUser = AuthUtils.getUser();	
 		Gson gson = new GsonBuilder().serializeNulls().create();
 		gson.serializeNulls();
 		Object body_aux = gson.fromJson(body, Object.class);
@@ -98,13 +97,13 @@ public class PerseoController {
 		if (body_map.get("rule") ==  null) 
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"A rule must be sent\"}");
 		String ruleJson = gson.toJson(body_map.get("rule"),LinkedTreeMap.class);
-//		if (body_map.get("user_id") == null) 
-//			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"A user_id must be sent\"}");
-//		String user_id =  body_map.get("user_id").toString();
+		if (body_map.get("user_id") == null) 
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"A user_id must be sent\"}");
+		String user_id =  body_map.get("user_id").toString();
 		String description = "no description";
 		if (body_map.get("description") != null)
 			description = body_map.get("description").toString();
-		String response = logic.parseAdvancedRule(ruleJson, loggedUser.getId().toString(), description);
+		String response = logic.parseAdvancedRule(ruleJson, user_id, description);
 		System.out.println(response);
 		if (response.contains("\"201\":\"created\""))
 			return ResponseEntity.status(HttpStatus.CREATED).body(response);
@@ -113,11 +112,10 @@ public class PerseoController {
 	}
 	
 	// Delete Methods 
-	@RequestMapping(value = "/statements", method = RequestMethod.DELETE, headers= {"Accept=application/json"})
+	@RequestMapping(value = "/statements/{user_id}", method = RequestMethod.DELETE, headers= {"Accept=application/json"})
 	@ResponseBody
-	public ResponseEntity deleteRule(@RequestParam("rule_name") String rule_name) {
-		auth.model.User loggedUser = AuthUtils.getUser();	
-		String response = logic.deleteRuleAndSubscription(loggedUser.getId().toString(), rule_name);
+	public ResponseEntity deleteRule(@PathVariable("user_id") String user_id,  @RequestParam("rule_name") String rule_name) {
+		String response = logic.deleteRuleAndSubscription(user_id, rule_name);
 		if (response.contains("\"error\" : \"Rule does not exist\""))
 			return new ResponseEntity(response, HttpStatus.BAD_REQUEST);
 		else
